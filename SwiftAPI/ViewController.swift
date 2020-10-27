e9bf760b188e8d0b5d7d3027226dbcacc36f18ad
Bill Mårtensson <bill@magictechnology.se>
2020-10-27 09:23:51 +0100

    Initial Commit

---
+++ SwiftAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAPI
+//
+//  Created by Bill Martensson on 2020-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
